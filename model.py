99448d26c617d712b8207e75bffcac99fce5b5bb
Jayant Chhikara <23f1000883@ds.study.iitm.ac.in>
2026-03-25 22:51:38 +0530

    25-03-2026

--- model.py
+++ model.py
@@ -22,6 +22,8 @@ class Company(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
     industry = db.Column(db.String(100), nullable=True)
     location = db.Column(db.String(120), nullable=True)
     website = db.Column(db.String(255), nullable=True)
@@ -40,6 +42,7 @@ class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     full_name = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
     phone = db.Column(db.String(20), nullable=True)
     course = db.Column(db.String(100), nullable=True)
     graduation_year = db.Column(db.Integer, nullable=True)
